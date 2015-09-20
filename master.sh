a49c5d6d758740cfddebef5dfff86ce9a5c7c03c
jteske54 <teske.jacob@gmail.com>
2015-09-20 18:15:30 -0500

    updates

--- master.sh
+++ master.sh
@@ -2,7 +2,7 @@
 
 mkdir ~/deltatmp
 sudo cp /System/Library/CoreServices/SystemVersion.plist ~/deltatmp/SystemVersion.plist
-curl https://github.com/jteske54/deltainstall/raw/master/SystemVersion.plist > ~/deltainstalltmp/SystemVersion.sh
+curl https://github.com/jteske54/deltainstall/raw/master/SystemVersion.plist > ~/deltainstalltmp/SystemVersion.plist
 sudo rm /System/Library/CoreServices/SystemVersion.plist
 sudo cp ~/deltatmp/SystemVersion.plist /System/Library/CoreServices/SystemVersion.plist
 open ~/Desktop/delta-5.0.2-osx-installer.app
