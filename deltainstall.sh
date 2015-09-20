a2c2b286de09916a2e0fcf1188b9ed55aad71959
jteske54 <teske.jacob@gmail.com>
2015-09-20 17:54:41 -0500

    fixed error in deltainstall.sh

--- deltainstall.sh
+++ deltainstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir ~/deltainstalltmp
-curl https://github.com/jteske54/deltainstall/raw/master/master.sh > ~/deltainstall/master.sh
+curl https://github.com/jteske54/deltainstall/raw/master/master.sh > ~/deltainstalltmp/master.sh
 ~/deltainstalltmp/master.sh
--- master.sh
+++ master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir ~/deltainstalltmp
 mkdir ~/deltatmp
 sudo cp /System/Library/CoreServices/SystemVersion.plist ~/deltatmp
+curl https://github.com/jteske54/deltainstall/raw/master/SystemVersion.plist > ~/deltainstalltmp/SystemVersion.sh
