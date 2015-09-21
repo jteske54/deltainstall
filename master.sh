5517505d14d131296a8318e3c178971cfa65587a
jteske54 <teske.jacob@gmail.com>
2015-09-20 21:48:47 -0500

    fixed problem where if you don't have the installer, SystemVersion.plist won't get put back.

--- master.sh
+++ master.sh
@@ -3,9 +3,9 @@
 mkdir ~/deltatmp
 sudo cp /System/Library/CoreServices/SystemVersion.plist ~/deltatmp/SystemVersion.plist
 curl https://raw.githubusercontent.com/jteske54/deltainstall/master/SystemVersion.plist > ~/deltainstalltmp/SystemVersion.plist
+open ~/Desktop/delta-5.0.2-osx-installer.app
 sudo rm /System/Library/CoreServices/SystemVersion.plist
 sudo cp ~/deltainstalltmp/SystemVersion.plist /System/Library/CoreServices/SystemVersion.plist
-open ~/Desktop/delta-5.0.2-osx-installer.app
 sleep 80
 sudo rm /System/Library/CoreServices/SystemVersion.plist
 sudo cp ~/deltatmp/SystemVersion.plist /System/Library/CoreServices/SystemVersion.plist
